2020551d5c402fc581ed303a564752f07b4496a1
JoshuaPruitt <JoshuaPruitt6484@gmail.com>
2024-07-22 22:48:56 -0600

    fixed information append bug in index.js

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 import fs from 'fs'
 import inquirer from 'inquirer'
 
-let name;
 
-const data = 
-`<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-</head>
-
-<body>
-    <h1>${name}</h1>
-    
-</body>
-</html>`
 
 inquirer
     .prompt([
@@ -28,10 +13,24 @@ inquirer
     ])
     .then((prompt) => {
 
-        name = prompt.name;
+        const data = 
+        `<!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <title></title>
+        </head>
+
+        <body>
+            <h1>${prompt.name}</h1>
+
+    
+        </body>
+        </html>`
 
         fs.writeFile('index.html', data, (err) =>
-            err ? console.error(err) : console.log('Success!'))
+            err ? console.error(err) : console.log('Success!'));
         
         }   
     );   
